2c20c3f6ad5e9fd79351363f4eeca3fda9a9e585
AmandaH <amanda.henz@unochapeco.edu.br>
2024-11-21 21:23:31 -0300

    Register medicine

--- MedicaAe/frontend/MedicaAe/src/pages/SystemPages/RegisterMedicine/RegisterMedicine.jsx
+++ MedicaAe/frontend/MedicaAe/src/pages/SystemPages/RegisterMedicine/RegisterMedicine.jsx
@@ -1,29 +1,50 @@
 import styles from "./RegisterMedicine.module.css";
 
 import { InputWithLabel } from "../../../components/Inputs/InputWithLabel";
-import { Input } from "../../../components/Inputs/Input";
+import { Textarea } from "../../../components/Inputs/Textarea";
 import { NavBar } from "../../../components/NavBar/NavBar";
 
 export const RegisterMedicine = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
   return (
-    <div className={styles.container}>
+    <div className={styles.page}>
         <NavBar/>
-        <div className={styles.subcontainer}>
+        <div className={styles.subpage}>
+            <div className={styles.left_box}>
             <h1 className={styles.RegisterMedicine_title}> Cadastrar medicamento </h1>
-            <form onSubmit={handleSubmit}>
-          <InputWithLabel label={"Indicação"} placeholder={"Digite o nome do medicamento"} />
+            <form className={styles.inputs}>
+              <div className={styles.InputWithLabel}>
+          
+              <InputWithLabel label={"Indicação"} placeholder={"Digite o nome do medicamento"} />
 
-          <InputWithLabel label={"Dosagem"} placeholder={"Digite a dosagem"} />
-
-          <InputWithLabel label={"Descrição (opcional)"} placeholder={"Adicione a descrição"} />
-        </form>
-        <button submit className={styles.save_button}>
-          Salvar
-        </button>
+              <InputWithLabel label={"Dosagem"} placeholder={"Digite a dosagem"} />
+              </div> 
+          </form>
+          <div className={styles.radios}>
+              <div className={styles.type} id="type">
+                <label htmlFor="type">Genérico ou Oririnal?</label>
+                <div>
+                  <div>
+                    <input type="radio" name="type" id="generico" />
+                    <label htmlFor="generico">Genérico</label>
+                  </div>
+                  <div>
+                    <input type="radio" name="type" id="original" />
+                    <label htmlFor="original">Original</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div className={styles.textarea}>
+            <Textarea
+              tittle={"Descrição (opcional)"}
+              placeholder={"Adicione a descrição do medicamento"}
+            />
+          </div>
+          <div className={styles.button}>
+              <button className={styles.save_button}> Salvar </button>
+          </div>
         </div>
     </div>
   )
-}
+};
